4859cc865bdad5a612aa9c57eb210c69da815f69
Lunici <67332249+Lunici@users.noreply.github.com>
2022-04-15 16:17:18 +0200

    Delete JuegoDeLaVida/src/GUI directory

--- JuegoDeLaVida/src/GUI/Juego.java
+++
@@ -1,9 +0,0 @@
-package GUI;
-import GUI.Frame.Ventana;
-
-public class Juego {
-      public static Ventana ventana = new Ventana(80, 50);
-      public static void main(String[] args) {
-            ventana.crearVentana();
-      }
-}
